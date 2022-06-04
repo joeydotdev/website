1f0bd312de7b5d840a3141438f28143e3a99998e
Joey Colon <contact@joey.dev>
2022-06-03 21:21:52 -0700

    feat: first iteration of website (#1)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,26 +1,31 @@
 import * as React from 'react';
 
+import UnderlineLink from '@/components/links/UnderlineLink';
 import UnstyledLink from '@/components/links/UnstyledLink';
 
 const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  { href: '/work', label: 'work' },
+  { href: '/posts', label: 'posts' },
+  { href: 'https://github.com/joeydotdev/website', label: 'source' },
 ];
 
 export default function Header() {
   return (
     <header className='sticky top-0 z-50 bg-white'>
       <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
+        <UnstyledLink
+          href='/'
+          className='text-2xl font-bold hover:text-gray-600'
+        >
+          joey.dev
         </UnstyledLink>
         <nav>
           <ul className='flex items-center justify-between space-x-4'>
             {links.map(({ href, label }) => (
               <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
+                <UnderlineLink href={href} className='hover:text-gray-600'>
                   {label}
-                </UnstyledLink>
+                </UnderlineLink>
               </li>
             ))}
           </ul>
