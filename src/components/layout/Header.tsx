df2653afe47ca95cce72cd80266c257b9dad69de
Joey Colon <contact@joey.dev>
2023-02-12 14:48:44 -0800

    feat: add blog page

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import ButtonLink from '@/components/links/ButtonLink';
 
 const links = [
-  { href: '/music', label: 'Music' },
+  { href: '/blog', label: 'Blog' },
   { href: 'https://github.com/joeydotdev', label: 'Github' },
   { href: 'https://www.linkedin.com/in/~joey/', label: 'Linkedin' },
 ];
